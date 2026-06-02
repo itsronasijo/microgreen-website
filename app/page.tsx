c69463fa766b9430871f1d3aa65dbea0ee575f59
itsronasijo <itsronasijo@gmail.com>
2026-06-02 11:59:18 +0530

    Replace Home component return with 'Hello'

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,3 @@
 export default function Home() {
-return (
-
-
-
-
-    <h1 className="text-5xl font-bold text-green-400 mb-6">
-      MicroGreen
-    </h1>
-
-    <p className="text-gray-400 text-xl">
-      Premium Organic Microgreens Website
-    </p>
-
-  </div>
-</main>
-
-);
+return Hello;
 }
