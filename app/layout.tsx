c6577bb74831c145fcd5192603ceba3af3294f0d
itsronasijo <itsronasijo@gmail.com>
2026-06-12 21:20:58 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <body>
          <TopBar />
         <Header />
-         <main className="pt-36">
+         <main className="pt-40">
         {children}
          </main>
       </body>
