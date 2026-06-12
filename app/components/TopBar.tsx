62c71f8e24ff24cbb4b4d5cbafc12ed9c771c689
itsronasijo <itsronasijo@gmail.com>
2026-06-12 18:21:29 +0530

    Update TopBar.tsx

--- app/components/TopBar.tsx
+++ app/components/TopBar.tsx
@@ -2,8 +2,16 @@
 
 export default function TopBar() {
   return (
-    <div className="bg-[#14532D] text-white p-3">
-      HELLO WORLD
+    <div className="bg-[#14532D] text-white">
+      <div className="px-8 py-2 flex justify-between text-sm">
+        <div>
+          Instagram | Facebook | WhatsApp
+        </div>
+
+        <div>
+          Fresh Delivery | App Coming Soon
+        </div>
+      </div>
     </div>
   );
 }
