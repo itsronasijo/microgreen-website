2f91bf2782c6d53d410cf3f29ac86420a4a216ee
itsronasijo <itsronasijo@gmail.com>
2026-06-12 18:09:56 +0530

    Update TopBar.tsx

--- app/components/TopBar.tsx
+++ app/components/TopBar.tsx
@@ -3,8 +3,7 @@
 export default function TopBar() {
   return (
     <div className="bg-[#14532D] text-white text-sm">
-      <div className="max-w-7xl mx-auto px-6 h-10 flex items-center justify-between">
-
+      <div className="w-full px-8 h-10 flex items-center justify-between">
         <div className="flex items-center gap-6">
           <span>Instagram</span>
           <span>Facebook</span>
